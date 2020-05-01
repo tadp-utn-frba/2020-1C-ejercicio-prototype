fb67f4cd30676e7f36a96559ebe8d8af8d65c502
juanFds <juan9794@gmail.com>
2020-05-01 19:32:20 -0300

    Definir y obtener propiedades 'simples'

--- spec/prototype_spec.rb
+++ spec/prototype_spec.rb
@@ -2,3 +2,39 @@
 # method missing
 # instance_eval.... -> instance_exec -> define_singleton_method(name, &block)
 # extender con call_next ?
+
+class PrototypedObject
+  def initialize
+    @properties = {}
+  end
+
+  def set_property(name, value)
+    @properties[name] = value
+  end
+
+  def get_property(name)
+    @properties.fetch(name) do
+      raise PropertyNotFoundError.new
+    end
+  end
+end
+
+class PropertyNotFoundError < StandardError
+end
+
+describe do
+  it '' do
+    guerrero = PrototypedObject.new
+
+    guerrero.set_property(:energia, 100)
+
+    expect(guerrero.get_property(:energia)).to eq(100)
+  end
+
+  it '' do
+    guerrero = PrototypedObject.new
+
+    expect { guerrero.get_property(:energia) }.to raise_error(PropertyNotFoundError)
+  end
+end
+
