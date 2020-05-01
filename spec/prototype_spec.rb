d72ea73034b47e700a9d825079da6fbec6d1b140
juanFds <juan9794@gmail.com>
2020-05-01 19:32:41 -0300

    Obtener propiedades por envío de mensaje con nombre de la propiedad

--- spec/prototype_spec.rb
+++ spec/prototype_spec.rb
@@ -17,13 +17,25 @@ class PrototypedObject
       raise PropertyNotFoundError.new
     end
   end
+
+  def method_missing(name, *params, &block)
+    if respond_to_missing?(name)
+      get_property(name)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(name, include_private_methods = false)
+    @properties.key?(name)
+  end
 end
 
 class PropertyNotFoundError < StandardError
 end
 
-describe do
-  it '' do
+describe 'Objetos prototipicos' do
+  it 'pueden definir propiedades y accederlas' do
     guerrero = PrototypedObject.new
 
     guerrero.set_property(:energia, 100)
@@ -31,10 +43,26 @@ describe do
     expect(guerrero.get_property(:energia)).to eq(100)
   end
 
-  it '' do
+  it 'fallan si se intenta acceder una propiedad que no poseen' do
     guerrero = PrototypedObject.new
 
     expect { guerrero.get_property(:energia) }.to raise_error(PropertyNotFoundError)
+  end
+
+  it 'puede responder mensajes cuyo nombre se corresponda a una propiedad definida' do
+    guerrero = PrototypedObject.new
+
+    guerrero.set_property(:energia, 100)
+
+    expect(guerrero.energia).to eq(100)
+    expect(guerrero.respond_to?(:energia)).to be(true)
+  end
+
+  it 'no puede responder mensajes cuyo nombre no se corresponda a ninguna propiedad definida' do
+    guerrero = PrototypedObject.new
+
+    expect { guerrero.energia }.to raise_error(NoMethodError)
+    expect(guerrero.respond_to?(:energia)).to be(false)
   end
 end
 
